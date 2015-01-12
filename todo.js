0a8441ef44fa736aac966753cd2ba039c9c8766b
Mendow <godovanyuk.mi@gmail.com>
2015-01-12 15:50:03 +0200

    some done

--- todo.js
+++ todo.js
@@ -1,8 +1,93 @@
+
+
+function addList(myTable) {
+    'use strict';
+    var table = document.getElementById(myTable),
+        rowCount = table.rows.length,
+        row = table.insertRow(rowCount),
+        b = document.getElementById('txt1').value,
+        cell1 = row.insertCell(0),
+        element1 = document.createElement("input"),
+        cell2 = row.insertCell(1),
+        element2 = document.createElement("input"),
+        cell3 = row.insertCell(2),
+        element3 = document.createElement("div");
+    element1.type = "text";
+    element1.style.width = "100%";
+    element1.name = "txtbox[]";
+    element1.value = b;
+    cell1.appendChild(element1);
+
+    element2.type = "checkbox";
+    element2.name = "chkbox[]";
+    element2.onclick = function () {
+        showDone(this);
+    }
+    cell2.appendChild(element2);
+
+    element3.innerHTML = "|&#10007|";
+    element3.onclick = function () {
+        taskDel(this);
+    }
+    cell3.appendChild(element3);
+
+    document.getElementById('txt1').value = " ";
+
+    document.getElementById('taskTotal').textContent = rowCount + 1;
+}
+
+function deleteMarked(myTable) {
+
+    var table = document.getElementById(myTable);
+    var rowCount = table.rows.length;
+
+    for (var i = 0; i < rowCount; i++) {
+        var row = table.rows[i];
+        var chkbox = row.cells[1].childNodes[0];
+        if (null != chkbox && true == chkbox.checked) {
+            table.deleteRow(i);
+            rowCount--;
+            i--;
+
+            document.getElementById('taskTotal').textContent = rowCount;
+
+        }
+    }
+}
+
+function markAll(myTable) {
+    var table = document.getElementById(myTable);
+    var rowCount = table.rows.length;
+
+    for (var i = 0; i < rowCount; i++) {
+        var row = table.rows[i];
+        row.cells[1].childNodes[0].checked = true;
+
+    }
+}
+
+function taskDel(t) {
+    var p = t.parentNode.parentNode;
+    p.parentNode.removeChild(p);
+    document.getElementById('taskTotal').textContent -= 1;
+}
+
+function showDone(t) {
+
+    if (t.checked) {
+        document.getElementById('taskDone').textContent = parseInt(document.getElementById('taskDone').textContent)+1;
+    } else {
+        document.getElementById('taskDone').textContent = parseInt(document.getElementById('taskDone').textContent)-1;
+    }
+}
+
+
 //&#10004; tick
 //&#10007; cross
 /*global alert */
 /*global console */
-
+/*gloabl document*/
+/*
 var newtask = document.getElementById("newtask");
 
 function divFunction() {
@@ -44,4 +129,4 @@ function taskDel(t) {
     'use strict';
     t.parentNode.parentNode.removeChild(t.parentNode);
 }
-alert("j");
+*/
